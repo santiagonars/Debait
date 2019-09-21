b667c82b8a7cdc9ccc5ae0c88ebaca9050aa179e
Santiago Norena <san.norena@gmail.com>
2019-09-21 16:05:01 -0400

    http Request to url

--- TextExtraction.py
+++ TextExtraction.py
@@ -13,4 +13,24 @@
 import io
 import os
 import requests
+from requests.exceptions import HTTPError, Timeout
 from bs4 import BeautifulSoup
+
+
+url = "https://www.vice.com/en_us/article/59nyjq/another-republican-senator-who-snubbed-trump-could-be-in-trouble"
+
+def httpRequest():
+    # Use requests to issue a standard HTTP GET 
+    try:
+        result = requests.get(url ,timeout=10)
+        # raise_for_status will throw an exception if an HTTP error
+        result.raise_for_status
+        print(result)
+    except HTTPError as err:
+        print("Error: {0}".format(err))
+    except Timeout as err:
+        print("Request time out {0}".format(err))
+
+
+if __name__ == '__main__':
+    httpRequest()

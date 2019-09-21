7b9e1b7049304f302355a2bd5af5c8c6892f966c
Santiago Norena <san.norena@gmail.com>
2019-09-21 11:46:36 -0400

    Update Sentiment_Analysis_NLP_GCP.py
    - Imports the Google Cloud client library
    - Load API Key to Access Google Cloud Platform
    - CLOUD VISION API EXAMPLE

--- Sentiment_Analysis_NLP_GCP.py
+++ Sentiment_Analysis_NLP_GCP.py
@@ -1,1 +1,60 @@
-# Google cloud connection
+# --------------------STEPS:------------------------------
+# Step 1: pip3 install --upgrade google-cloud-language
+
+# --------------------NOTES:------------------------------
+# NOTE: 
+
+
+# --------------------BUGS:------------------------------
+# BUG: 
+
+
+
+import io
+import os
+#import json
+#import pandas as pd
+
+
+# ----------Imports the Google Cloud client library----------
+# Cloud Vision Library
+from google.cloud import vision
+# Natural Lanaguage Processing Libraries (NLP)
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+
+# ---------Load API Key to Access Google Cloud Platform----------
+#***IMPORTANT: make sure JSON file for service account key name is correct & that it's inside the authPath directory
+serviceKey = "CloudVision-sandbox-366681a0e85d.json"  
+#print("Service Key= " + serviceKey)
+
+with open(serviceKey, 'r') as myfile:
+    json_authCred=myfile.read()
+    # print(json_authCred)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = serviceKey
+#print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+
+
+
+# ---------CLOUD VISION API EXAMPLE---------
+
+URLlink = "https://www.epiloglaser.com/resources/sample-club/images/baby-blocks/baby-blocks-thumb.jpg"
+
+client = vision.ImageAnnotatorClient() # Instantiate a client to use the vision method 
+image = vision.types.Image()
+image.source.image_uri = URLlink
+
+# Perform label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+for label in labels:
+    labelList = []
+    labelList = label.description
+    print(labelList)
+
+
+
